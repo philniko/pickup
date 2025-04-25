a1fceb517b79039d783414af588ce111940d5da5
Philippe Nikolov <philippe.nikolov@outlook.com>
2025-04-25 17:03:27 -0400

    renamed events screen

--- app/(tabs)/events.tsx
+++ app/(tabs)/events.tsx
@@ -1,7 +1,7 @@
 import { Text, View, StyleSheet } from 'react-native';
 import COLORS from '../constants/colors';
 
-export default function FriendsScreen() {
+export default function EventsScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Events screen</Text>
